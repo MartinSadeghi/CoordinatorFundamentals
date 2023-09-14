a0cd196679e0048dfd64640ba42fa4b2d4c25617
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-09-14 12:48:23 +0200

    Scene func in the SceneDelegate changed according to the Coordinator.

---
+++ CoordinatorFundamentals/Main ViewController/ApplicationCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  ApplicationCoordinator.swift
+//  CoordinatorFundamentals
+//
+//  Created by Mommo Sadeghi on 14/09/23.
+//
+
+import Foundation
+
+
+class ApplicationCoordinator: Coordinator {
+    
+    func start() {
+        <#code#>
+    }
+    
+    
+}
---
+++ CoordinatorFundamentals/Protocols/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  CoordinatorFundamentals
+//
+//  Created by Mommo Sadeghi on 14/09/23.
+//
+
+import Foundation
+
+
+protocol Coordinator {
+    func start() 
+}
