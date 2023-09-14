12c4163e671343cee53fdde30341daf90aed1117
moMMo Sadeghi <37694875+moMMoSadeghi@users.noreply.github.com>
2023-09-14 18:14:53 +0200

    First and Second ViewControllers added,

---
+++ CoordinatorFundamentals/Main/First/FirstDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstDetailView.swift
+//  CoordinatorFundamentals
+//
+//  Created by Mommo Sadeghi on 14/09/23.
+//
+
+import SwiftUI
+
+struct FirstDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstDetailView()
+    }
+}
